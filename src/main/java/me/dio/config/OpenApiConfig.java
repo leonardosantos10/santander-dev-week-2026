f5111f3a5299050a4596933d26bae5aa0b7ed5b5
Leonardo Santos <seuemail@gmail.com>
2026-03-10 10:29:17 -0300

    Implement user management features and update dependencies

---
+++ src/main/java/me/dio/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package me.dio.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Santander Dev Week 2026 API")
+                        .version("1.0.0")
+                        .description("Java RESTful API criada para Santander Dev Week 2026")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
--- src/main/java/me/dio/domain/model/Feature.java
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -1,9 +1,6 @@
 package me.dio.domain.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 
 @Entity(name = "tb_feature")
